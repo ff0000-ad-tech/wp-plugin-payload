4a8343536203cbdf2922fbec84482c815573ab75
Greg Connell <greg.connell@ff0000.com>
2017-11-20 18:37:26 -0800

    wrapping up beta of multi-use abstraction

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ function WpPluginPayload(deploy, options) {
 					assets: {
 						source: this.deploy.settings.assets.images,
 						importPath: `./${deploy.env.paths.ad.images}`
-					}
+					},
+					target: (gets added by the plugin, from compiler.options.entry[entry.name])
 				},{
 					entry: 'font',
 					assets: {
 						source: this.deploy.settings.assets.images,
 						importPath: `../${deploy.env.paths.common.context}/${deploy.env.paths.common.fonts}`
-					}
+					},
+					target: (gets added by the plugin, from compiler.options.entry[entry.name])
 				}]
 			}
 	*/
@@ -41,9 +43,6 @@ WpPluginPayload.prototype.apply = function (compiler) {
 	// on compiler entry (happens once)
 	compiler.plugin('entry-option', (compilation, callback) => {
 		log('Preparing payload management...');
-
-		// force initial compile to happen
-		this.deploy.payload.recompile = true;
 
 		// prepare options
 		this.prepareOptions(compiler);
@@ -105,15 +104,22 @@ WpPluginPayload.prototype.watchSettings = function (compiler, compilation) {
  *	If any of the payload-modules have been updated, the payload needs to be recompiled
  */
 WpPluginPayload.prototype.watchPayloadModules = function (compiler, compilation) {
-	for (var watchFile in compilation.fileTimestamps) {
-		for (var i in this.deploy.payload.modules) {
-			if (this.hasUpdate(compilation, watchFile, this.deploy.payload.modules[i].userRequest)) {
-				log(`payload modules have changed - RECOMPILE`);
-				this.deploy.payload.recompile = true;
-				return; // only one change is needed to flag recompile
-			}
+	// each payload entry
+	this.options.entries.forEach((entry) => {
+		// if entry-modules have been determined
+		if (this.options.output[entry.name].modules) {
+			for (var watchFile in compilation.fileTimestamps) {
+				// for each entry-module
+				for (var m in this.options.output[entry.name].modules) {
+					if (this.hasUpdate(compilation, watchFile, this.options.output[entry.name].modules[m].userRequest)) {
+						log(`'${entry.name}' modules have changed - RECOMPILE`);
+						this.options.output[entry.name].recompile = true;
+						return; // only one change is needed to flag recompile for this entry
+					}
+				}
+			}			
 		}
-	}
+	});
 }
 
 
@@ -133,17 +139,25 @@ WpPluginPayload.prototype.prepareOptions = function (compiler) {
 		}
 		else {
 			log(`Cannot watch/compile '${entry.name}'!! No entry specified at 'compiler.options.entry.${entry.name}'`);
-			this.deploy.payload[entry.name].compile = false;
+			this.options.payload[entry.name].compile = false;
 		}
 	})
+
 	// map entry-target to each request
 	.map((entry) => {
+		// get the full path to the entry
 		entry.target = compiler.options.entry[entry.name];
+
+		// force initial compile to happen
+		this.options.output[entry.name].recompile = true;
+		this.options.output[entry.name].modules = [];
+
 		return entry;
 	});
 
 	log(this.options);
 }
+
 
 // update imports
 WpPluginPayload.prototype.updatePayloadImports = function (compiler) {
@@ -173,21 +187,21 @@ WpPluginPayload.prototype.hasUpdate = function (compilation, watchFile, requestF
 
 // utility to rebuild payload dependencies list
 WpPluginPayload.prototype.refreshPayloadModules = function (compiler, compilation) {
-	if (!compiler.options.entry.payload) { return; }
+	log('Refreshing payload modules:');
+	this.options.entries.forEach((entry) => {
+		this.options.output[entry.name].modules = [];
 
-	log(compiler.options.module.rules);
+		const dependencies = compilation._modules[entry.target].dependencies;
+		dependencies.forEach((dependency) => {
+			if (dependency.constructor.name == 'HarmonyImportDependency') {
+				// log(module.module._source)
+				this.options.output[entry.name].modules.push(
+					dependency.module
+				);
+			}
+		});
 
-	log('Refreshing payload modules');
-	this.deploy.payload.modules = [];
-	const dependencies = compilation._modules[compiler.options.entry.payload].dependencies;
-	dependencies.forEach((dependency) => {
-		if (dependency.constructor.name == 'HarmonyImportDependency') {
-			// log(module.module._source)
-			this.deploy.payload.modules.push(
-				dependency.module
-			);
-		}
-	});	
+	});
 }
 
 
